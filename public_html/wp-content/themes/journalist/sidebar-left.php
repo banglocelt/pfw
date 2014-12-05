cac70baeea8c4eace37cd452ccbd9a62a6c3ccc7
banglocelt <banglocelt@gmail.com>
2014-12-05 00:11:18 +0000

    December 2014 Best moved up

--- public_html/wp-content/themes/journalist/sidebar-left.php
+++ public_html/wp-content/themes/journalist/sidebar-left.php
@@ -109,8 +109,17 @@ email address as link
 	<div class="superSponsors"> BEST OF THE BEST</div> <br/> 
 	<div class="somePersonalFavourites">SOME PERSONAL FAVOURITES</div>
 	    
-    <ul>	
-	    <li><a href="http://www.provencefoodandwine.com/2012/01/13/chateau-la-coste/" title="CH&Acirc;TEAU LA COSTE">
+    <ul>			
+		
+		<li><a href="http://www.provencefoodandwine.com/2012/08/23/kelly-mcauliffe-wine-tours/" title="KELLY McAULIFFE WINE TOURS">
+		<div id="best5KellyMcAuliffeWineTours"></div> </a> <div style="position:relative;width:180px; padding-top:3px;" >KELLY McAULIFFE WINE TOURS</div> <br/></li>
+		<div id="postOrWebsite">
+			<ul>
+				<li>Go To ->  <a href="http://www.provencefoodandwine.com/2012/08/23/kelly-mcauliffe-wine-tours/" title="Read the Post"> Post</a> </li>
+			</ul>		
+		</div>
+		
+		<li><a href="http://www.provencefoodandwine.com/2012/01/13/chateau-la-coste/" title="CH&Acirc;TEAU LA COSTE">
 		<div id="best6ChateauLaCoste"></div> </a> <div style="position:relative;padding-top:3px;class="recentPosts">CH&Acirc;TEAU LA COSTE</div> <br/></li>
 		<div id="postOrWebsite">
 			<ul>
@@ -119,7 +128,7 @@ email address as link
 			</ul>
 		
 		</div>
-		
+
 		<li><a href="http://www.provencefoodandwine.com/2010/06/25/domaine-du-grapillon-dor/" title="DOMAINE DU GRAPILLON D’OR">
 				<div id="best7DomaineDuGrap"></div> </a> <div style="position:relative;padding-top:3px">DOMAINE DU GRAPILLON D'OR</div> <br/> </li>
 		<div id="postOrWebsite">
@@ -169,13 +178,7 @@ email address as link
 		
 		</div>
 
-		<li><a href="http://www.provencefoodandwine.com/2012/08/23/kelly-mcauliffe-wine-tours/" title="KELLY McAULIFFE WINE TOURS">
-		<div id="best5KellyMcAuliffeWineTours"></div> </a> <div style="position:relative;width:180px; padding-top:3px;" >KELLY McAULIFFE WINE TOURS</div> <br/></li>
-		<div id="postOrWebsite">
-			<ul>
-				<li>Go To ->  <a href="http://www.provencefoodandwine.com/2012/08/23/kelly-mcauliffe-wine-tours/" title="Read the Post"> Post</a> </li>
-			</ul>		
-		</div>
+				
 		
 	</ul>
 	
